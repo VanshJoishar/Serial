30e93b1c22e76ec29fe4d8e19bb64c16cd6ee9db
Vansh Joishar <vanshjoishar@gmail.com>
2022-01-23 02:58:47 +0530

    Update

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+def generate_nonce():
+    serial = int.from_bytes(os.urandom(10), byteorder="big")
+    print (serial)
+    return serial
+
+@app.route("/", methods=["POST"])
+def index():
+    return jsonify(serial = int(generate_nonce()))
