0f12075f828d7fb70ab103153591e0fdd5c2cb56
Vansh Joishar <vanshjoishar@gmail.com>
2022-01-23 05:24:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,6 +13,6 @@ def generate_nonce():
 #    if request.method=='POST':
 #        return jsonify(message = "Welcome to eth signin, please sign this message to confirm.", nonce = int(generate_nonce()))
 
-@app.route("/nonce",  methods=['GET'])
+@app.route("/",  methods=['GET'])
 def index():
     return jsonify(nonce = int(generate_nonce()))
